ea9f12f72aa1562368c500d3166598f4c0a9783c
Ignatz <iggy@m3lt.work>
2025-12-10 14:45:26 +0100

    make toa suite leak free

--- src/ftt_lib_toa.c
+++ src/ftt_lib_toa.c
@@ -20,7 +20,6 @@
 
 #define T(a) Test(libft_toa, a)
 
-
 T(ulto_bl_easy_0)
 {
   unsigned long nb = 0;
@@ -29,36 +28,45 @@ T(ulto_bl_easy_0)
   cr_assert_eq(result_size, 1);
   target[1] = '\0';
   cr_assert_str_eq(target, "0");
+  free(target);
 }
 
 T(ulto_bl_easy_1)
 {
   unsigned long nb = 1;
   char *target = ftt_malloc(2 * sizeof(char));
-  size_t result_size = ft_ultoa_bl(nb, target, 1, "01");
-  cr_assert_eq(result_size, 1);
+  cr_assert_eq(ft_ultoa_bl(nb, target, 1, "01"), 1);
   target[1] = '\0';
   cr_assert_str_eq(target, "1");
+  free(target);
 }
 
 T(ltoa_b_easy_pos)
 {
-  cr_assert_str_eq(ft_ultoa_b(4, FT_ALPHABET_DEC), "4");
+  char *s;
+  cr_assert_str_eq(s = ft_ultoa_b(4, FT_ALPHABET_DEC), "4");
+  free(s);
 }
 
 T(ltoa_b_easy_42)
 {
-  cr_assert_str_eq(ft_ultoa_b(42, FT_ALPHABET_DEC), "42");
+  char *s;
+  cr_assert_str_eq(s = ft_ultoa_b(42, FT_ALPHABET_DEC), "42");
+  free(s);
 }
 
 T(ltoa_b_easy_neg)
 {
-  cr_assert_str_eq(ft_ltoa_b(-4, FT_ALPHABET_DEC), "-4");
+  char *s;
+  cr_assert_str_eq(s = ft_ltoa_b(-4, FT_ALPHABET_DEC), "-4");
+  free(s);
 }
 
 T(ltoa_b_zero)
 {
-  cr_assert_str_eq(ft_ltoa_b(0, FT_ALPHABET_DEC), "0");
+  char *s;
+  cr_assert_str_eq(s = ft_ltoa_b(0, FT_ALPHABET_DEC), "0");
+  free(s);
 }
 
 T(ltoa_bl_easy_neg)
@@ -68,63 +76,76 @@ T(ltoa_bl_easy_neg)
 
 T(ltoa_b_neg_min)
 {
-  char *expected = ft_str_alloc(10);
+  char *expected = ft_str_alloc(20);
   sprintf(expected, "%ld", LONG_MIN);
   char *actual = ft_ltoa_b(LONG_MIN, FT_ALPHABET_DEC);
   cr_assert_str_eq(expected, actual);
+  free(expected);
+  free(actual);
 }
 
 T(ltoa_b_pos_max)
 {
-  char buffer[100];
-  char *expected = (char *) &buffer;
+  char *expected = ft_str_alloc(20);
   sprintf(expected, "%ld", LONG_MAX);
   char *actual = ft_ultoa_b(LONG_MAX, FT_ALPHABET_DEC);
   cr_assert_str_eq(expected, actual);
+  free(actual);
+  free(expected);
 }
 
 T(itoa_simple)
 {
-	cr_assert_str_eq("42", ft_itoa(42));
-	cr_assert_str_eq("-1", ft_itoa(-1));
-	cr_assert_str_eq("0", ft_itoa(0));
+  char *s;
+	cr_assert_str_eq(s = ft_itoa(42),"42" ); free(s);
+	cr_assert_str_eq(s = ft_itoa(-1), "-1"); free(s);
+	cr_assert_str_eq(s = ft_itoa(0),"0"); free(s);
 }
 
 T(itoa_binary_base)
 {
-  cr_assert_str_eq(ft_itoa_b(1, "0X"), "X");
-  cr_assert_str_eq(ft_itoa_b(0, "0X"), "0");
+  char *s;
+  cr_assert_str_eq(s = ft_itoa_b(1, "0X"), "X"); free(s);
+  cr_assert_str_eq(s = ft_itoa_b(0, "0X"), "0"); free(s);
 }
 
 T(itoa_binary_base_larg)
 {
-  cr_assert_str_eq(ft_itoa_b(2, "01"), "10");
-  cr_assert_str_eq(ft_itoa_b(42, "01"), "101010");
-  cr_assert_str_eq(ft_itoa_b(4, "0X"), "X00");
+  char *s;
+  cr_assert_str_eq(s = ft_itoa_b(2, "01"), "10"); free(s);
+  cr_assert_str_eq(s = ft_itoa_b(42, "01"), "101010"); free(s);
+  cr_assert_str_eq(s = ft_itoa_b(4, "0X"), "X00"); free(s);
 }
 
 T(itoa_INT_MAX)
 {
 	char *s_int_max = (char *) malloc(15 * sizeof(char));
+	char *s;
 	snprintf(s_int_max, 15, "%d", INT_MAX);
-	cr_assert_str_eq(s_int_max, ft_itoa(INT_MAX));
+	cr_assert_str_eq(s = ft_itoa(INT_MAX), s_int_max);
+	free(s);
+	free(s_int_max);
 }
 
 T(btoa_FF)
 {
 	char *s = ft_btoa(0xFF);
 	cr_assert_str_eq(s, "FF");
+	free(s);
 }
 
 T(btoa_A0)
 {
 	char *s = ft_btoa(0xA0);
 	cr_assert_str_eq(s, "A0");
+	free(s);
 }
 
 T(btoa_b_max_value)
 {
-  cr_assert_str_eq(ft_btoa_b(-1, "01"), "11111111");
+  char *s;
+  cr_assert_str_eq(s = ft_btoa_b(-1, "01"), "11111111");
+  free(s);
 }
 
 
